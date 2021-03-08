68d493c567c1c02527ad5687a3e062320c331ee9
Genry Leyva <gleyvaglez@gmail.com>
2021-03-08 18:39:17 -0300

    github actions

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "genryhelloazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
 ### ---------- RESOURCE GROUP ---------------
 
 resource "azurerm_resource_group" "rgfrontend" {
